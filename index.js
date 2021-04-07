18c74749ec9223d9baa713e25332632c5d3b715e
Jared de Uriarte <jared.deuri@gmail.com>
2021-04-07 18:16:50 -0400

    added mysql connection

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 //Requiring npm inquirer
 const inquirer = require('inquirer')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: 'root',
+    // Your password
+    password: '',
+    database: 'employee_tracker_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}\n`);
+    // run the start function after the connection is made to prompt the user
+});
+
+// const afterConnection = () => {
+//     connection.query('SELECT * FROM songs', (err, res) => {
+//       if (err) throw err;
+//       console.table(res);
+//     });
+//   };
+
+
+
+
 
 // Questions for inquirer prompt
 const questions = [
     {
-        name: 'choose',
         type: 'list',
         message: 'Select an action',
-        choices: ['View employee data', 'Add employee data', 'Update employee data']
+        choices: ['View employee data', 'Add employee data', 'Update employee data'],
+        name: 'action'
 
     }
 ]
+
+
+
+
+
+
+
+
+
+
+
 
 //Initializes inquirer prompts
 function init() {
@@ -18,8 +58,8 @@ function init() {
         //Accepts response from questions
         .then(response => {
             console.log(response)
-            //If the response is _____, then do appropriate function
-            switch (response.choose) {
+            //If the response is _____, then do appropriate function.
+            switch (response.action) {
                 case 'View employee data':
                     viewEmployee()
                     break;
@@ -32,8 +72,9 @@ function init() {
             }
         })
 
-}
+};
 
+// use connection.query (SELECT ____ FROM ______) in the below functions
 function viewEmployee() {
     console.log('---View employee---')
 };
@@ -43,8 +84,6 @@ function addEmployee() {
 function updateEmployee() {
     console.log('---Update employee---')
 };
-
-
 
 //Calls init function
 init();
