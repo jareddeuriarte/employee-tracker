eb130eb518537c791c3a9058e73c32d57e313473
Jared de Uriarte <jared.deuri@gmail.com>
2021-04-07 00:18:21 -0400

    testing switch case

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
+//Requiring npm inquirer
 const inquirer = require('inquirer')
 
-
+// Questions for inquirer prompt
 const questions = [
     {
-        name:'choose',
+        name: 'choose',
         type: 'list',
         message: 'Select an action',
-        choices: ['View employee data', 'Add employee data', 'Update employee data', ]
-
+        choices: ['View employee data', 'Add employee data', 'Update employee data']
     }
 ]
 
-
-
-
-function init(){
+//Initializes inquirer prompts
+function init() {
     inquirer.prompt(questions)
-    .then(response => {
-        console.log(response)
-    })
+        //Accepts response from questions
+        .then(response => {
+            console.log(response)
+            //If the response is _____, then do appropriate function
+            switch (response) {
+                case 'View employee data':
+                    console.log('---View employee---')
+                    // viewEmployee()
+                    break;
+                case 'Add employee data':
+                    console.log('---Add employee---')
+                    // addEmployee()
+                    break;
+                case 'Update employee data':
+                    console.log('---Update employee---')
+                    // updateEmployee()
+                    break;
+            }
+        })
 
 }
 
+
+function viewEmployee() {
+    console.log('---View employee---')
+};
+function addEmployee() {
+    console.log('---Add employee---')
+};
+function updateEmployee() {
+    console.log('---Update employee---')
+};
+
+
+
+//Calls init function
 init();
